e93d23184d4a8335baf22279475596fb44d5b421
橘子 <juzi@macmini.local>
2017-12-21 14:04:23 +0800

    12211404

---
+++ 1511/VC4.h
@@ -0,0 +1,13 @@
+//
+//  VC4.h
+//  1511
+//
+//  Created by LJ on 2017/12/21.
+//  Copyright © 2017年 lijie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VC4 : UIViewController
+
+@end
---
+++ 1511/VC4.m
@@ -0,0 +1,37 @@
+//
+//  VC4.m
+//  1511
+//
+//  Created by LJ on 2017/12/21.
+//  Copyright © 2017年 lijie. All rights reserved.
+//
+
+#import "VC4.h"
+
+@interface VC4 ()
+
+@end
+
+@implementation VC4
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
